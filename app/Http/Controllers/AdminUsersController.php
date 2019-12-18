b04936d29ccd84ea36591269754d386aac7b0987
sarojghimire2074@gmail.com <sarojghimire2074@gmail.com>
2019-12-18 17:39:01 +0545

    users with uploading feature working

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests;
 use App\Http\Requests\UsersRequest;
+use App\Photo;
 use App\Role;
 use App\User;
 use Illuminate\Http\Request;
@@ -48,9 +49,28 @@ class AdminUsersController extends Controller
     {
         //
 
-        User::create($request->all());
+        $input = $request->all();
 
-        return redirect('/admin/users');
+
+        if($file = $request->file('photo_id')) {
+
+            $name = time() . $file->getClientOriginalName();
+
+            $file->move('images', $name);
+
+            $photo = Photo::create(['file'=>$name]); 
+
+            $input['photo_id'] = $photo->id;
+
+        }
+
+        $input['password'] = bcrypt($request->password);
+
+        User::create($input);
+
+        // User::create($request->all());
+
+        // return redirect('/admin/users');
 
         // return $request->all();
     }
--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -10,32 +10,32 @@
 	{!! Form::open(['method'=>'POST', 'action'=> 'AdminUsersController@store','files'=>true]) !!}
 
 	 <div class="form-group">
-	 	{!! Form::label('name', 'Name') !!}
+	 	{!! Form::label('name', 'Name:') !!}
 	 	{!! Form::text('name', null, ['class'=>'form-control'])!!}
 	 </div>
 
 	 <div class="form-group">
-	 	{!! Form::label('email', 'Email') !!}
+	 	{!! Form::label('email', 'Email:') !!}
 	 	{!! Form::email('email', null, ['class'=>'form-control'])!!}
 	 </div>
 
 	  <div class="form-group">
-	 	{!! Form::label('role_id', 'Role') !!}
+	 	{!! Form::label('role_id', 'Role:') !!}
 	 	{!! Form::select('role_id', [''=>'Choose options'] + $roles , null, ['class'=>'form-control'])!!}
 	 </div>
 
 	  <div class="form-group">
-	 	{!! Form::label('is_active', 'Status') !!}
+	 	{!! Form::label('is_active', 'Status:') !!}
 	 	{!! Form::select('is_active', array(1 => 'Active', 0=> 'Not Active'), 0, ['class'=>'form-control'])!!}
 	 </div>
 
 	 <div class="form-group">
-	 	{!! Form::label('file', 'Email') !!}
-	 	{!! Form::file('file', null, ['class'=>'form-control'])!!}
+	 	{!! Form::label('photo_id', 'Photo:') !!}
+	 	{!! Form::file('photo_id', null, ['class'=>'form-control'])!!}
 	 </div>
 
 	 <div class="form-group">
-	 	{!! Form::label('password', 'Password') !!}
+	 	{!! Form::label('password', 'Password:') !!}
 	 	{!! Form::password('password', ['class'=>'form-control'])!!}
 	 </div>
 

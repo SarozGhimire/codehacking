42a1b496f41d189063a7699c001b6653b53d3ee9
sarojghimire2074@gmail.com <sarojghimire2074@gmail.com>
2019-12-22 16:30:00 +0545

    able to create categories

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -57,7 +57,7 @@ class AdminPostsController extends Controller
         $user = Auth::user();
 
         if ($file = $request->file('photo_id')) {
-            
+
             $name = time() . $file->getClientOriginalName();
 
             $file->move('images', $name);
@@ -94,7 +94,11 @@ class AdminPostsController extends Controller
     {
         //
 
-        return view('admin.posts.edit');
+        $post = Post::findOrFail($id);
+
+        $categories = Category::lists('name','id')->all();
+
+        return view('admin.posts.edit', compact('post','categories'));
     }
 
     /**
@@ -107,6 +111,28 @@ class AdminPostsController extends Controller
     public function update(Request $request, $id)
     {
         //
+
+
+
+        $input = $request->all(); 
+
+
+
+        if ($file = $request->file('photo_id')) {
+
+            $name = time() . $file->getClientOriginalName();
+
+            $file->move('images', $name);
+
+            $photo = Photo::create(['file'=>$name]);
+
+            $input['photo_id'] = $photo->id;
+        }
+
+
+        Auth::user()->posts()->whereId($id)->first()->update($input);
+
+        return redirect('/admin/posts');
     }
 
     /**
@@ -118,5 +144,15 @@ class AdminPostsController extends Controller
     public function destroy($id)
     {
         //
+
+        $post = Post::findOrFail($id);
+
+        $path = str_replace('..','', $post->photo->file);
+
+        unlink(public_path() . $path);
+
+        $post->delete();
+
+        return redirect('/admin/posts');
     }
 }
---
+++ resources/views/admin/categories/edit.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.admin')
+
+
+
+@section('content')
+
+
+<h1>Categories</h1>
+
+
+<div class="col-sm-6">
+
+
+    {!! Form::open(['method'=>'POST', 'action'=> ['AdminCategoriesController@update']]) !!}
+
+    <div class="form-group">
+        {!! Form::label('name', 'Name:') !!}
+        {!! Form::text('name', null, ['class'=>'form-control'])!!}
+    </div>
+
+
+    <div class="form-group">
+        {!! Form::submit('Create Category', ['class'=>'btn btn-primary'])!!}
+    </div>
+
+    {!! Form::close() !!}
+
+
+</div>
+
+
+
+
+@stop
